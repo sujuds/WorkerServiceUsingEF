7fdc4facd09d7674739a21d301190ec06914e195
Sujud Satwikayana <satwikayana@gmail.com>
2021-12-23 16:56:32 +0800

    init project

---
+++ WorkerServiceApp1/Services/DbHelper.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorkerServiceApp1.Models;
+
+namespace WorkerServiceApp1.Services
+{
+    public class DbHelper
+    {
+        private AppDbContext dbContext;
+
+        private DbContextOptions<AppDbContext> GetAllOptions()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionBuilder.UseMySql(AppSettings.ConnectionString, ServerVersion.AutoDetect(AppSettings.ConnectionString));
+            return optionBuilder.Options;
+        }
+
+
+        //GetAllUser
+        public List<User> GetAllUser()
+        {
+            using (dbContext = new AppDbContext(GetAllOptions()))
+            {
+                try
+                {
+                    var users = dbContext.Users.ToList();
+                    if (users != null)
+                    {
+                        return users;
+                    }
+                    else
+                    {
+                        return new List<User>();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        //Seed Data
+        //Used when no data is present, we want some default data to fill in the database
+        public void SeedData()
+        {
+            using(dbContext=new AppDbContext(GetAllOptions()))
+            {
+                dbContext.Users.Add(new User
+                {
+                    Name = "Sujud",
+                    Address = "Denpasar, Bali"
+                });
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
